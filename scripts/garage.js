3d6ca0ad584dc64a262d4e359025d1e2fa4d440b
devjanaprime <dev@primeacademy.io>
2016-11-29 14:43:57 -0600

    test button click in jQuery

--- scripts/garage.js
+++ scripts/garage.js
@@ -1,1 +1,9 @@
 console.log( 'js' );
+
+$( document ).ready( function(){
+  console.log( 'JQ' );
+  // button click event for testButton
+  $( '#testButton' ).on( 'click', function(){
+    console.log( 'button clicked...' );
+  }); // end on click for #testButton
+}); // end doc ready
