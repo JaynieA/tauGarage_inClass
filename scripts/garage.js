33758824a225ad6e654ec81bff1a8a16ecef0a1f
devjanaprime <dev@primeacademy.io>
2016-11-29 16:26:43 -0600

    user input working

--- scripts/garage.js
+++ scripts/garage.js
@@ -5,5 +5,15 @@ $( document ).ready( function(){
   // button click event for testButton
   $( '#parkCarButton' ).on( 'click', function(){
     console.log( 'parkCarButton clicked...' );
+
+    // get user input & save it in an object
+    var newCar ={
+      year: $( '#year' ).val(),
+      make: $( '#make' ).val(),
+      model: $( '#model' ).val(),
+      description: $( '#description' ).val(),
+      imageUrl: $( '#imageUrl' ).val()
+    }; // end newCar object  );
+    console.log( 'adding:', newCar );
   }); // end on click for #parkCarButton
 }); // end doc ready
